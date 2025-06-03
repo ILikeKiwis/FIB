7e5bb1ae106ba82296a63586006fd2ee9a1bfbc9
ILikeKiwis <krisi.stoykov@gmail.com>
2025-06-03 19:06:34 +0200

    Deffered Shading

---
+++ G/Viewer/plugins/deferred_shading/deferred_shading.cpp
@@ -0,0 +1,48 @@
+#include "deferred_shading.h"
+#include "glwidget.h"
+
+void Deferred_shading::onPluginLoad()
+{
+	
+}
+
+void Deferred_shading::preFrame()
+{
+	
+}
+
+void Deferred_shading::postFrame()
+{
+	
+}
+
+void Deferred_shading::onObjectAdd()
+{
+	
+}
+
+bool Deferred_shading::drawScene()
+{
+	return false; // return true only if implemented
+}
+
+bool Deferred_shading::drawObject(int)
+{
+	return false; // return true only if implemented
+}
+
+bool Deferred_shading::paintGL()
+{
+	return false; // return true only if implemented
+}
+
+void Deferred_shading::keyPressEvent(QKeyEvent *)
+{
+	
+}
+
+void Deferred_shading::mouseMoveEvent(QMouseEvent *)
+{
+	
+}
+
---
+++ G/Viewer/plugins/deferred_shading/deferred_shading.h
@@ -0,0 +1,29 @@
+#ifndef _DEFERRED_SHADING_H
+#define _DEFERRED_SHADING_H
+
+#include "plugin.h" 
+
+class Deferred_shading: public QObject, public Plugin
+{
+	Q_OBJECT
+	Q_PLUGIN_METADATA(IID "Plugin") 
+	Q_INTERFACES(Plugin)
+
+  public:
+	 void onPluginLoad();
+	 void preFrame();
+	 void postFrame();
+
+	 void onObjectAdd();
+	 bool drawScene();
+	 bool drawObject(int);
+
+	 bool paintGL();
+
+	 void keyPressEvent(QKeyEvent *);
+	 void mouseMoveEvent(QMouseEvent *);
+  private:
+	// add private methods and attributes here
+};
+
+#endif
